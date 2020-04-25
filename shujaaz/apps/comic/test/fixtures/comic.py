853807a62a50d03b7754ce029caf89dceafc1dd2
salmanyagaka <salmanyagaka@gmail.com>
2020-04-25 21:07:18 +0300

    Added tests

---
+++ shujaaz/apps/comic/test/fixtures/comic.py
@@ -0,0 +1,20 @@
+fetch_comics  = {
+    "pk": "1",
+    "model": "user.user",
+    "fields": {
+        "username": "salmanyagaka",
+        "first_name": "Salma",
+        "last_name": "Nyagaka",
+        "email": "salmanyagak@gmail.com"
+    }
+}
+
+
+fetch_specific_comic_response = "Successfully fetched the comic book"
+
+fetch_all_comics_response = "These are the Comics available"
+
+
+invalid_comic_id = {
+    "error":"This comic does not exist"
+}
