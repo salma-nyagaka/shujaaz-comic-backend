dfefbba7775a2ba7fb0399b261505ce76559a272
salmanyagaka <salmanyagaka@gmail.com>
2020-04-26 15:08:26 +0300

    Deleted secret key

--- shujaaz/settings.py
+++ shujaaz/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mi+xv3xefdu@3bt_&lxhhys-9b#(vkhu*_iot-+5&5h2p%+in9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
