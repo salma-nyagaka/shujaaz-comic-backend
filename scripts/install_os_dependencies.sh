e377233ac87428b08f6acb929293d09d774b1e1c
salmanyagaka <salmanyagaka@gmail.com>
2020-04-26 21:58:19 +0300

    Added aws configuration

--- scripts/install_os_dependencies.sh
+++ scripts/install_os_dependencies.sh
@@ -2,4 +2,3 @@
 yum install -y python-psycopg2 postgresql libncurses5-dev libffi libffi-devel libxml2-devel libxslt-devel libxslt1-dev
 yum install -y postgresql-libs postgresql-devel python-lxml python-devel gcc patch python-setuptools
 yum install -y gcc-c++ flex epel-release nginx supervisor
-yum install virtualenv
