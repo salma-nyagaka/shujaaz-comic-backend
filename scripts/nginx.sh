85be3cc1ddba0d0a29167c3aa2b4d7ca6e1e3158
salmanyagaka <salmanyagaka@gmail.com>
2020-04-27 00:59:50 +0300

    Added aws configuration

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -11,7 +11,7 @@ unlink /etc/nginx/sites-enabled/*
 
 cp /home/ec2-user/www/shujaaz-comic/nginx/staging.conf /etc/nginx/sites-available/my-project-host.conf
 
-# ln -s /etc/nginx/sites-available/my-project-host.conf /etc/nginx/sites-enabled/my-project-host.conf
+ln -s /etc/nginx/sites-available/my-project-host.conf /etc/nginx/sites-enabled/my-project-host.conf
 
 # /etc/init.d/nginx reload
 # /etc/init.d/nginx start
