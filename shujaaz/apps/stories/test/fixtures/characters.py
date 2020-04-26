1e870e23930aee774e49b24718f47b16d4402154
salmanyagaka <salmanyagaka@gmail.com>
2020-04-26 12:42:22 +0300

    Added fixtures for tests

---
+++ shujaaz/apps/stories/test/fixtures/characters.py
@@ -0,0 +1,14 @@
+fetch_character  =   {
+        "pk": "15",
+        "model": "stories.characters",
+        "fields": {
+            "name": "froyo",
+            "story_id": "2"
+        }
+    }
+
+fetch_character_response = "Successfully fetched this characters in this story"
+
+invalid_story_id = {
+    "error":"This story has no characters"
+}
