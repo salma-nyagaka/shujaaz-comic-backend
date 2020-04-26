80fc4d5b37c2804cb10f16e4ef40a8ecf0ce016a
salmanyagaka <salmanyagaka@gmail.com>
2020-04-26 22:30:30 +0300

    Added aws configuration

--- shujaaz/settings.py
+++ shujaaz/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'mi+xv3xefdu@3bt_&lxhhys-9b#(vkhu*_iot-+5&5h2p%+in9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 env = environ.Env(
     # set casting, default value
@@ -86,22 +86,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'shujaaz.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'shujaaz',
-#         'USER': 'shujaaz',
-#         'PASSWORD': '',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#         'TEST': {
-#             'NAME': 'shujaaz_test_db',
-#         },
-#     }
-# }
 
 DATABASES = {
     # read the database environ
