5829c91c5d2cea3c54edce1525d9ca7b06536032
salmanyagaka <salmanyagaka@gmail.com>
2020-04-28 01:01:46 +0300

    Added apache server

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -2,8 +2,8 @@
 chown ubuntu:ubuntu /home/ubuntu/www
 apt-get install python3-pip libapache2-mod-wsgi-py3
 pip3 install virtualenv
-virtualenv /home/ubuntu/www/project-venv
-chown ubuntu:ubuntu /home/ubuntu/www/project-venv
-chown ubuntu:ubuntu /home/ubuntu/www/project-venv/*
-source /home/ubuntu/www/project-venv/bin/activate
+virtualenv /home/ubuntu/www/shujaaz-comic/venv
+chown ubuntu:ubuntu /home/ubuntu/www/shujaaz-comic/venv
+chown ubuntu:ubuntu /home/ubuntu/www/shujaaz-comic/venv/*
+source /home/ubuntu/www/shujaaz-comic/venv/bin/activate
 pip3 install -r /home/ubuntu/www/shujaaz-comic/requirements.txt
