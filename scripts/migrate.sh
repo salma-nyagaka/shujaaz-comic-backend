0ef0bf180fdbe3a91c7b5db29158ac90405d8676
salmanyagaka <salmanyagaka@gmail.com>
2020-04-28 01:10:49 +0300

    Added apache server

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,6 +2,10 @@
 cd /home/ubuntu/www/shujaaz-comic/
 source /home/ubuntu/www/shujaaz-comic/venv/bin/activate
 service postgresql start
+-u postgres psql
+create role shujaaz
+create database shujaaz 
+\q
 DJANGO_SETTINGS_MODULE=shujaaz.settings SECRET_KEY="mi+xv3xefdu@3bt_&lxhhys-9b#(vkhu*_iot-+5&5h2p%+in9" PSQL_DB_NAME=shujaaz PSQL_DB_USER=salma PSQL_DB_PASSWD="shujaaz" PSQL_HOST=localhost PSQL_PORT=5432 DATABASE_URL=postgres://shujaaz:''@localhost:5432/shujaaz ./manage.py makemigrations
 DJANGO_SETTINGS_MODULE=shujaaz.settings SECRET_KEY="mi+xv3xefdu@3bt_&lxhhys-9b#(vkhu*_iot-+5&5h2p%+in9" PSQL_DB_NAME=shujaaz PSQL_DB_USER=salma PSQL_DB_PASSWD="shujaaz" PSQL_HOST=localhost PSQL_PORT=5432 DATABASE_URL=postgres://shujaaz:''@localhost:5432/shujaaz ./manage.py migrate
 DJANGO_SETTINGS_MODULE=shujaaz.settings SECRET_KEY="mi+xv3xefdu@3bt_&lxhhys-9b#(vkhu*_iot-+5&5h2p%+in9" PSQL_DB_NAME=shujaaz PSQL_DB_USER=salma PSQL_DB_PASSWD="shujaaz" PSQL_HOST=localhost PSQL_PORT=5432 DATABASE_URL=postgres://shujaaz:''@localhost:5432/shujaaz ./manage.py loaddata /home/ubuntu/www/shujaaz-comic/shujaaz/apps/user/fixtures/user
