6200cbbed8389014769b6050c1156fe7758ae8e6
salmanyagaka <salmanyagaka@gmail.com>
2020-04-28 05:12:21 +0300

    Added apache server

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -6,7 +6,7 @@ psql -d shujaaz -U www-data -W
 psql -d shujaaz -U shujaaz -W
 
 
-# -u postgres createuser www-data
+-u postgres createuser root
 # -u postgres createdb -O shujaaz postgres
 DJANGO_SETTINGS_MODULE=shujaaz.settings SECRET_KEY="mi+xv3xefdu@3bt_&lxhhys-9b#(vkhu*_iot-+5&5h2p%+in9" PSQL_DB_NAME=shujaaz PSQL_DB_USER=www-data PSQL_DB_PASSWD='shujaaz' PSQL_HOST=localhost PSQL_PORT=5432 
 DJANGO_SETTINGS_MODULE=shujaaz.settings SECRET_KEY="mi+xv3xefdu@3bt_&lxhhys-9b#(vkhu*_iot-+5&5h2p%+in9" PSQL_DB_NAME=shujaaz PSQL_DB_USER=www-data PSQL_DB_PASSWD='shujaaz' PSQL_HOST=localhost PSQL_PORT=5432 
