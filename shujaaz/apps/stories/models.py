fcd5f397aa08c584bdb4d3891844397745e2bf4a
salmanyagaka <salmanyagaka@gmail.com>
2020-04-28 03:53:24 +0300

    Added apache server

--- shujaaz/apps/stories/models.py
+++ shujaaz/apps/stories/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from shujaaz.apps.comic.models import Comic
+from shujaaz.apps.comic.models import comic
 
 
 class Stories(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=256, blank=True)
     paragraph = models.CharField(max_length=256, blank=True)
-    comic = models.ForeignKey(Comic, on_delete=models.CASCADE)
+    comic = models.ForeignKey(comic, on_delete=models.CASCADE)
