ad557c33cd20aecacc913e44eb3b0d4915c35c8a
salmanyagaka <salmanyagaka@gmail.com>
2020-04-30 00:13:21 +0300

    Updated index html

--- shujaaz/urls.py
+++ shujaaz/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
-from shujaaz.home import home_view  
 
 
 schema_view = get_swagger_view (title="Shujaaz Comics Documentation")
