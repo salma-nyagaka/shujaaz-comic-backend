fa1211f3c04b979fb8625ac49005169fbb85dfea
salmanyagaka <salmanyagaka@gmail.com>
2020-04-30 00:01:07 +0300

    Updated readme file

--- shujaaz/urls.py
+++ shujaaz/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
-from home import home_view  
+from shujaaz import home_view  
 
 
 schema_view = get_swagger_view (title="Shujaaz Comics Documentation")
