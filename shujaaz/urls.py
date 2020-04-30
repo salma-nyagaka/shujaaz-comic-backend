6a6a14f729c61675be3c5935895546563a16b564
salmanyagaka <salmanyagaka@gmail.com>
2020-05-01 01:56:03 +0300

    Updated documentation

--- shujaaz/urls.py
+++ shujaaz/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
 
-
-schema_view = get_swagger_view (title="Shujaaz Comics Documentation")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +28,6 @@ urlpatterns = [
         include('shujaaz.apps.comic.urls', namespace="comic")),
     path('api/',
         include('shujaaz.apps.stories.urls', namespace="stories")),
+    path('docs', include_docs_urls(title='Comic Api')),
+
 ]
